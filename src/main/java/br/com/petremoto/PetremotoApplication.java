b948b889709260ec741a09de5fe49c0e9063b98c
ArnaldoSena <arnaldoufcg@gmail.com>
2020-08-19 23:47:28 -0300

    first commit

---
+++ src/main/java/br/com/petremoto/PetremotoApplication.java
@@ -0,0 +1,13 @@
+package br.com.petremoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetremotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetremotoApplication.class, args);
+	}
+
+}
